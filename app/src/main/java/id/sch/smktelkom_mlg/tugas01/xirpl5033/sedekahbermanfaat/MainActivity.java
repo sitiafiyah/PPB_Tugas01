5aabf667f8c8d211e2cb8a223ebdf2d12db6ab24
Siti Afiyah <sitiafiyah35@gmail.com>
2016-09-16 04:24:55 +0700

    + Desain form + Inisialisasi + doProcess

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5033/sedekahbermanfaat/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5033/sedekahbermanfaat/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.tugas01.xirpl5033.sedekahbermanfaat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     RadioButton rbLK, rbPM;
     CheckBox cbCD, cbMB, cbMN, cbBR;
     Spinner spAN;
+    Button bOK;
     TextView tvHasil;
 
     @Override
@@ -40,5 +43,52 @@ public class MainActivity extends AppCompatActivity {
 
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
+        bOK = (Button) findViewById(R.id.buttonOK);
+        bOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doProcess();
+                doClick();
+            }
+        });
+
+    }
+
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            String kelas = etKelas.getText().toString();
+            tvHasil.setText(nama + " \n\n kelas " + kelas);
+        }
+    }
+
+    private void doClick() {
+
+    }
+
+    public boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        String kelas = etKelas.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum Anda isi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+
+        if (kelas.isEmpty()) {
+            etKelas.setError("Kelas belum diisi");
+            valid = false;
+        } else {
+            etKelas.setError(null);
+        }
+
+        return valid;
     }
 }
